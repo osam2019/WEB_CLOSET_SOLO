dff64b66690b193b41ed37602ead40ed0e382ddc
Hyung-Taik Choi <htc.refactor@gmail.com>
2019-10-23 16:02:56 +0900

    Added <nav> at <header> area

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <div>Icons made by <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn</a> from <a
+            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+    <div>Icons made by <a href="https://www.flaticon.com/authors/fjstudio" title="fjstudio">fjstudio</a> from <a
+            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 </template>
 
 <script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,38 @@
 <template>
     <div class="page">
         <div class="container">
-            <div class="header">
-                <img class="logo" src="../assets/logo.svg">
-                <h1>Closet</h1>
+            <div id="header">
+                <div class = "logo">
+                    <img class="logo-img" src="../assets/logo.svg">
+                    <h1>Closet</h1>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>My Closet</li>
+                        <li>Lookbooks</li>
+                        <li>Guides</li>
+                        <li>Find Items</li>
+                    </ul>
+                </nav>
                 <div class="clear"></div>
             </div>
 
-            <div>
-                Main content area
+            <main>
+                <table>
+                    <tr>
+                        <td rowspan = "2">Accordion Menu Area</td>
+                        <td>Cover Flow Menu Area</td>
+                    </tr>
+
+                    <tr>
+                        <td>Cover Flow Menu Area</td>
+                    </tr>
+                </table>
+            </main>
+
+            <div id = "footer">
+                Footer content area
             </div>
         </div>
     </div>
