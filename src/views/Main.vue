7e3ba130e96d88cbe4148daad21b59bcee0247df
Hyung-Taik Choi <htc.refactor@gmail.com>
2019-10-24 14:51:00 +0900

    Retouched design

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -21,7 +21,7 @@
                 <table>
                     <tr>
                         <td id="collapse" rowspan="2">
-                            <div id = "menu-title">Tops</div>
+                            <div id = "collapse-title">Tops</div>
                             <el-collapse v-model="activeNames" @change="handleChange" accordion>
                                 <el-collapse-item title="Color" name="tops-color">
                                     <input type="checkbox" id="tops-color-black" name="Black" value="black">
@@ -60,7 +60,7 @@
                                 </el-collapse-item>
                             </el-collapse>
 
-                            <div id = "menu-title">Tops</div>
+                            <div id = "collapse-title">Bottoms</div>
                             <el-collapse v-model="activeNames" @change="handleChange" accordion>
                                 <el-collapse-item title="Color" name="bottoms-color">
                                     <input type="checkbox" id="tops-color-black" name="Black" value="black">
@@ -100,14 +100,14 @@
                             </el-collapse>
                         </td>
                         <td class = "coverflow-area">
-                            <coverflow :coverList="coverListBottomsDenims" :width="1250" :coverWidth="300"
+                            <coverflow :coverList="coverListTopsCardigans" :width="1250" :coverWidth="250"
                                        :index="10" style="margin: 0 auto"></coverflow>
                         </td>
                     </tr>
 
                     <tr>
                         <td class = "coverflow-area">
-                            <coverflow :coverList="coverListBottomsDenims" :width="1250" :coverWidth="300"
+                            <coverflow :coverList="coverListBottomsDenims" :width="1250" :coverWidth="250"
                                        :index="10" style="margin: 0 auto"></coverflow>
                         </td>
                     </tr>
@@ -141,165 +141,165 @@
                 coverListTopsCardigans: [
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/8SECONDS_269X5ADY1A.png',
-                        title: '8SECONDS_269X5ADY1A'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/COSTUME%20O%E2%80%99CLOCK_COOSCD001BEIGE.png',
-                        title: 'COSTUME O’CLOCK_COOSCD001BEIGE'
+                        title: 'COSTUME O’CLOCK'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/STOCKHOLM%20SYNDROME_S93UKL06-BLACK.png',
-                        title: 'STOCKHOLM SYNDROME_S93UKL06-BLACK'
+                        title: 'STOCKHOLM SYNDROME'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/TBJ%20ADVANCED_T194KC720P-GRAY.png',
-                        title: 'TBJ ADVANCED_T194KC720P-GRAY'
+                        title: 'TBJ ADVANCED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/VOIEBIT_V546.png',
-                        title: 'VOIEBIT_V546'
+                        title: 'VOIEBIT'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/8SECONDS_269X5ADY1A.png',
-                        title: '8SECONDS_269X5ADY1A'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/COSTUME%20O%E2%80%99CLOCK_COOSCD001BEIGE.png',
-                        title: 'COSTUME O’CLOCK_COOSCD001BEIGE'
+                        title: 'COSTUME O’CLOCK'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/STOCKHOLM%20SYNDROME_S93UKL06-BLACK.png',
-                        title: 'STOCKHOLM SYNDROME_S93UKL06-BLACK'
+                        title: 'STOCKHOLM SYNDROME'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/TBJ%20ADVANCED_T194KC720P-GRAY.png',
-                        title: 'TBJ ADVANCED_T194KC720P-GRAY'
+                        title: 'TBJ ADVANCED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/VOIEBIT_V546.png',
-                        title: 'VOIEBIT_V546'
+                        title: 'VOIEBIT'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/8SECONDS_269X5ADY1A.png',
-                        title: '8SECONDS_269X5ADY1A'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/COSTUME%20O%E2%80%99CLOCK_COOSCD001BEIGE.png',
-                        title: 'COSTUME O’CLOCK_COOSCD001BEIGE'
+                        title: 'COSTUME O’CLOCK'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/STOCKHOLM%20SYNDROME_S93UKL06-BLACK.png',
-                        title: 'STOCKHOLM SYNDROME_S93UKL06-BLACK'
+                        title: 'STOCKHOLM SYNDROME'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/TBJ%20ADVANCED_T194KC720P-GRAY.png',
-                        title: 'TBJ ADVANCED_T194KC720P-GRAY'
+                        title: 'TBJ ADVANCED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/VOIEBIT_V546.png',
-                        title: 'VOIEBIT_V546'
+                        title: 'VOIEBIT'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/8SECONDS_269X5ADY1A.png',
-                        title: '8SECONDS_269X5ADY1A'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/COSTUME%20O%E2%80%99CLOCK_COOSCD001BEIGE.png',
-                        title: 'COSTUME O’CLOCK_COOSCD001BEIGE'
+                        title: 'COSTUME O’CLOCK'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/STOCKHOLM%20SYNDROME_S93UKL06-BLACK.png',
-                        title: 'STOCKHOLM SYNDROME_S93UKL06-BLACK'
+                        title: 'STOCKHOLM SYNDROME'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/TBJ%20ADVANCED_T194KC720P-GRAY.png',
-                        title: 'TBJ ADVANCED_T194KC720P-GRAY'
+                        title: 'TBJ ADVANCED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/tops/cardigans/VOIEBIT_V546.png',
-                        title: 'VOIEBIT_V546'
+                        title: 'VOIEBIT'
                     }
                 ],
                 coverListBottomsDenims: [
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/8SECONDS_269X21DP2R.png',
-                        title: '8SECONDS_269X21DP2R'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/BRANDED_LOT1931.png',
-                        title: 'BRANDED_LOT1931'
+                        title: 'BRANDED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/MODIFIED_M%230720.png',
-                        title: 'MODIFIED_M#0720'
+                        title: 'MODIFIED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH0DPTB15612.png',
-                        title: 'PIECE WORKER_PMH0DPTB15612'
+                        title: 'PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH3DPTB01002.png',
-                        title: '1PIECE WORKER_PMH3DPTB01002'
+                        title: 'PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/8SECONDS_269X21DP2R.png',
-                        title: '8SECONDS_269X21DP2R'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/BRANDED_LOT1931.png',
-                        title: 'BRANDED_LOT1931'
+                        title: 'BRANDED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/MODIFIED_M%230720.png',
-                        title: 'MODIFIED_M#0720'
+                        title: 'MODIFIED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH0DPTB15612.png',
-                        title: 'PIECE WORKER_PMH0DPTB15612'
+                        title: 'PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH3DPTB01002.png',
-                        title: '1PIECE WORKER_PMH3DPTB01002'
+                        title: '1PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/8SECONDS_269X21DP2R.png',
-                        title: '8SECONDS_269X21DP2R'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/BRANDED_LOT1931.png',
-                        title: 'BRANDED_LOT1931'
+                        title: 'BRANDED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/MODIFIED_M%230720.png',
-                        title: 'MODIFIED_M#0720'
+                        title: 'MODIFIED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH0DPTB15612.png',
-                        title: 'PIECE WORKER_PMH0DPTB15612'
+                        title: 'PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH3DPTB01002.png',
-                        title: '1PIECE WORKER_PMH3DPTB01002'
+                        title: '1PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/8SECONDS_269X21DP2R.png',
-                        title: '8SECONDS_269X21DP2R'
+                        title: '8SECONDS'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/BRANDED_LOT1931.png',
-                        title: 'BRANDED_LOT1931'
+                        title: 'BRANDED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/MODIFIED_M%230720.png',
-                        title: 'MODIFIED_M#0720'
+                        title: 'MODIFIED'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH0DPTB15612.png',
-                        title: 'PIECE WORKER_PMH0DPTB15612'
+                        title: 'PIECE WORKER'
                     },
                     {
                         cover: 'https://raw.githubusercontent.com/htcrefactor/closet-web/master/public/images/men/bottoms/denims/PIECE%20WORKER_PMH3DPTB01002.png',
-                        title: '1PIECE WORKER_PMH3DPTB01002'
+                        title: 'PIECE WORKER'
                     }
                 ]
             };
@@ -313,21 +313,44 @@
 </script>
 
 <style scoped>
-    #collapse {
-        width: 25%;
+    header {
+        background-color: #2D2F36;
     }
 
-    #menu-title {
-        font-family: 'Inter UI', sans-serif;
-        font-weight: 900;
+    nav {
+        background-color: transparent;
+    }
+
+    #collapse {
+        width: 25%;
+        margin-right: 20px;
     }
 
     table {
         margin: 20px;
     }
 
-    #collapse {
-        margin-right: 20px;
+    #collapse-title {
+        font-family: 'Inter UI', sans-serif;
+        font-weight: 900;
+        margin: 20px 0 10px 0;
+    }
+
+    footer  {
+        padding-top: 8px;
+    }
+
+    .icon-credits {
+        color: #e2e2e5;
+        text-align: right;
+        font-style: italic;
+
+    }
+
+    .icon-credits a {
+        text-decoration: none;
+        color: #e2e2e5;
+        text-underline: none;
     }
 
 </style>
